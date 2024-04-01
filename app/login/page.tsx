f271d9b15b69f1ea8e2a3e80eddf0e2e6247ddf4
takubii <vicsat93@gmail.com>
2024-04-01 18:40:07 +0900

    add: ログインページのデザインを追加

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,15 +3,25 @@ import Link from 'next/link';
 
 export default function LoginPage() {
   return (
-    <>
-      <div>
-        ログインページ
+    <main className='flex justify-center md:h-screen'>
+      <div className='flex flex-col relative items-center w-full max-w-[400px]'>
+        <h1 className='my-6 w-full text-center text-2xl'>ログインページ</h1>
         <LoginForm />
+        <div className='flex flex-col mt-8 text-center'>
+          <Link
+            href='/register'
+            className='bg-blue-500 text-white rounded-lg px-8 py-2 hover:bg-blue-400 focus-visible:outline-offset-2'
+          >
+            ユーザー登録
+          </Link>
+          <Link
+            href='/'
+            className='bg-blue-500 text-white rounded-lg px-8 py-2 mt-2 hover:bg-blue-400 focus-visible:outline-offset-2'
+          >
+            ホーム
+          </Link>
+        </div>
       </div>
-      <div className='flex flex-col w-full mt-2'>
-        <Link href='/register'>ユーザー登録</Link>
-        <Link href='/'>ホーム</Link>
-      </div>
-    </>
+    </main>
   );
 }
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -7,17 +7,44 @@ export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(login, undefined);
 
   return (
-    <form action={dispatch}>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input id='email' type='email' name='email' placeholder='メールアドレス' required />
+    <form action={dispatch} className='w-full'>
+      <div className='w-full rounded-lg bg-gray-50 pt-6 pb-4 px-6 '>
+        <div className='w-full'>
+          <div>
+            <label htmlFor='email' className='block mb-2 text-gray-800'>
+              Email
+            </label>
+            <input
+              className='block w-full rounded-md border border-gray-200 pl-2 py-2 outline-2'
+              id='email'
+              type='email'
+              name='email'
+              placeholder='メールアドレス'
+              required
+            />
+          </div>
+
+          <div className='mt-4'>
+            <label htmlFor='password' className='block mb-2 text-gray-800'>
+              Password
+            </label>
+            <input
+              className='block w-full rounded-md border border-gray-200 pl-2 py-2 outline-2'
+              id='password'
+              type='password'
+              name='password'
+              placeholder='パスワード'
+              required
+            />
+          </div>
+        </div>
+        <button className='mt-8 w-full rounded-lg bg-blue-500 text-white h-10 hover:bg-blue-400 focus-visible:outline-offset-2'>
+          ログイン
+        </button>
+        <div className='flex h-8 items-end space-x-1'>
+          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+        </div>
       </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input id='password' type='password' name='password' placeholder='パスワード' required />
-      </div>
-      <div>{errorMessage && <p>{errorMessage}</p>}</div>
-      <button>ログイン</button>
     </form>
   );
 }
