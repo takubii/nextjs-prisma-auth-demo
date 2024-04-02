0bedf243ccfa45cfe5cec8fff7a0c2b1a38e479e
takubii <vicsat93@gmail.com>
2024-04-02 11:44:24 +0900

    add: TOPページのデザインを追加

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,42 @@ export default async function Home() {
   const session = await auth();
 
   return (
-    <main className='flex min-h-screen flex-col items-center p-24'>
+    <main className='flex min-h-screen flex-col items-center'>
       {session ? (
-        <>
-          <Link href='/mypage'>マイページ</Link>
-          <form action={logout}>
-            <button>ログアウト</button>
-          </form>
-        </>
+        <div className='flex flex-col items-center'>
+          <h1 className='my-6 w-full text-center text-2xl'>TOPページ</h1>
+          <div className='flex flex-col text-center'>
+            <Link
+              href='/mypage'
+              className='bg-blue-500 text-white rounded-lg px-8 py-2 hover:bg-blue-400 focus-visible:outline-offset-2'
+            >
+              マイページ
+            </Link>
+            <form action={logout}>
+              <button className='bg-red-500 text-white rounded-lg px-8 py-2 mt-2 hover:bg-red-400 focus-visible:outline-offset-2'>
+                ログアウト
+              </button>
+            </form>
+          </div>
+        </div>
       ) : (
-        <>
-          <Link href='/login'>ログイン</Link>
-          <Link href='/register'>サインアップ</Link>
-        </>
+        <div className='flex flex-col items-center'>
+          <h1 className='my-6 w-full text-center text-2xl'>認証ページ</h1>
+          <div className='flex flex-col text-center'>
+            <Link
+              href='/login'
+              className='bg-blue-500 text-white rounded-lg px-8 py-2 hover:bg-blue-400 focus-visible:outline-offset-2'
+            >
+              ログイン
+            </Link>
+            <Link
+              href='/register'
+              className='bg-blue-500 text-white rounded-lg px-8 py-2 mt-2 hover:bg-blue-400 focus-visible:outline-offset-2'
+            >
+              サインアップ
+            </Link>
+          </div>
+        </div>
       )}
     </main>
   );
