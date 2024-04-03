62752f40be18b9f6a57924de705a402dcbfc098d
takubii <vicsat93@gmail.com>
2024-04-03 18:21:46 +0900

    update: middlewareで認証ページ等の切り替え

--- auth.config.ts
+++ auth.config.ts
@@ -1,30 +1,8 @@
-import { DEFAULT_LOGIN_REDIRECT, authRoutes, publicRoutes } from '@/routes';
 import { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
   pages: {
     signIn: '/login',
-  },
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-      const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-
-      if (isAuthRoute) {
-        if (isLoggedIn) {
-          return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-        }
-
-        return true;
-      }
-
-      if (!isPublicRoute && !isLoggedIn) {
-        return false;
-      }
-
-      return true;
-    },
   },
   providers: [],
 } satisfies NextAuthConfig;
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,29 @@
 import { authConfig } from '@/auth.config';
+import { DEFAULT_LOGIN_REDIRECT, authRoutes, publicRoutes } from '@/routes';
 import NextAuth from 'next-auth';
 
-export default NextAuth(authConfig).auth;
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/login', nextUrl));
+  }
+
+  return;
+});
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
